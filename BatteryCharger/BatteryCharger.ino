9b5bf454a023cba666e535e4e29399bbe12f2364
david parker <17parker@gmail.com>
2020-08-01 20:02:56 -0700

    InitalMaser

---
+++ BatteryCharger/BatteryCharger.ino
@@ -0,0 +1,98 @@
+#include "arrayOp.h"
+#include "TM1637Display.h"
+//basically all of this until the end of setup() is relay pins related
+const int relay1 = 9;
+const int relay2 = 8;
+const int relay3 = 7;
+const int relay4 = 6;
+const int relay5 = 5;
+const int relay6 = 4;
+const int relay7 = 3;
+const int relay8 = 2;
+int relayPairs[4][2] = { {relay1, relay5}, {relay2, relay6}, {relay3, relay7}, {relay4, relay8} };
+//These are for dad's voltage probe and pwm signal and ammeter pin
+const int pwmPin = 10;
+const int probePin = A0;
+const int ampPin = A7;
+//prototypes
+void cellSearch();
+void stop();
+const uint8_t noBattery[4] = { 0b01010100,0b01011100,0b00000000,0b01111100 };
+//The one and only useless waste of memory
+ArrayOpClass bogusObj(relayPairs);
+TM1637Display countdown = TM1637Display(11, 12);
+
+void setup() {
+    pinMode(relay1, OUTPUT);
+    pinMode(relay2, OUTPUT);
+    pinMode(relay3, OUTPUT);
+    pinMode(relay4, OUTPUT);
+    pinMode(relay5, OUTPUT);
+    pinMode(relay6, OUTPUT);
+    pinMode(relay7, OUTPUT);
+    pinMode(relay8, OUTPUT);
+    pinMode(pwmPin, OUTPUT);
+    digitalWrite(relay1, HIGH);
+    digitalWrite(relay2, HIGH);
+    digitalWrite(relay3, HIGH);
+    digitalWrite(relay4, HIGH);
+    digitalWrite(relay5, HIGH);
+    digitalWrite(relay6, HIGH);
+    digitalWrite(relay7, HIGH);
+    digitalWrite(relay8, HIGH);
+    digitalWrite(pwmPin, HIGH);
+    analogWrite(pwmPin, 255);
+    //For the countdown display
+    countdown.setBrightness(4, 1);
+    for (int tim = 0; tim <= 100; tim++) {
+        countdown.showNumberDec(100 - tim, 0, 4, 0);
+    }
+}
+
+//Makes bogusObj and sends that to the cpp file where the bulk of the work is done
+//IDK why I am doing it this way but it's fun I guess
+void loop() {
+    int* chargeArray;
+    bogusObj.probeInputPin = probePin;
+    bogusObj.pwmOutputPin = pwmPin;
+    bogusObj.ampInputPin = ampPin;
+    cellSearch();
+    bogusObj.masterSweep(bogusObj);
+}
+//self explanatory
+void stop() {
+    countdown.clear();
+    countdown.setSegments(noBattery);
+    while (1);
+}
+
+//find the cells that need to be charged, else stop() program
+//"returns the array" of which cells are low, if none need charging stop() the program
+void cellSearch() {
+    int chargeSum = 0;
+    static int c[4] = { 0, 0, 0, 0 };
+    bool okToChargeFlag = false;
+    for (int i = 0; i <= 3; i++) {
+        digitalWrite(bogusObj.relayPins[i][0], LOW);
+        digitalWrite(bogusObj.relayPins[i][1], LOW);
+        delay(150);
+        chargeSum = 0.0;
+        for (int probeLoop = 0; probeLoop <= 3; probeLoop++) {
+            chargeSum += analogRead(probePin);
+        }
+        digitalWrite(bogusObj.relayPins[i][0], HIGH);
+        digitalWrite(bogusObj.relayPins[i][1], HIGH);
+        bogusObj.voltArray[i] = chargeSum;
+        if (chargeSum < 840 && chargeSum > 550) {
+            bogusObj.chargePos[i] = 1;
+            okToChargeFlag = true;
+        }
+        if (chargeSum >= 840 || chargeSum <= 550) {
+            bogusObj.chargePos[i] = 0;
+        }
+        delay(150);
+    }
+    if (okToChargeFlag == false)
+        stop();
+    return;
+}
---
+++ BatteryCharger/Debug/BatteryCharger.cpp.d
@@ -0,0 +1,19 @@
+C:\Users\17par\AppData\Local\Temp\VMBuilds\BatteryCharger\nano_atmega328\Debug\BatteryCharger.cpp.o: \
+ C:\Users\17par\AppData\Local\Temp\VMBuilds\BatteryCharger\nano_atmega328\Debug\BatteryCharger.cpp \
+ C:\Program\ Files\ (x86)\Arduino\hardware\arduino\avr\cores\arduino/arduino.h \
+ C:\Program\ Files\ (x86)\Arduino\hardware\arduino\avr\cores\arduino/binary.h \
+ C:\Program\ Files\ (x86)\Arduino\hardware\arduino\avr\cores\arduino/WCharacter.h \
+ C:\Program\ Files\ (x86)\Arduino\hardware\arduino\avr\cores\arduino/WString.h \
+ C:\Program\ Files\ (x86)\Arduino\hardware\arduino\avr\cores\arduino/HardwareSerial.h \
+ C:\Program\ Files\ (x86)\Arduino\hardware\arduino\avr\cores\arduino/Stream.h \
+ C:\Program\ Files\ (x86)\Arduino\hardware\arduino\avr\cores\arduino/Print.h \
+ C:\Program\ Files\ (x86)\Arduino\hardware\arduino\avr\cores\arduino/Printable.h \
+ C:\Program\ Files\ (x86)\Arduino\hardware\arduino\avr\cores\arduino/USBAPI.h \
+ C:\Program\ Files\ (x86)\Arduino\hardware\arduino\avr\cores\arduino/Arduino.h \
+ C:\Program\ Files\ (x86)\Arduino\hardware\arduino\avr\variants\eightanaloginputs/pins_arduino.h \
+ c:\program\ files\ (x86)\arduino\hardware\arduino\avr\variants\standard\pins_arduino.h \
+ c:\program\ files\ (x86)\microsoft\ visual\ studio\2019\community\common7\ide\extensions\ze3s3dil.pco\Micro\ Platforms\default\debuggers\VM_DBG/VM_DBG.h \
+ C:\Program\ Files\ (x86)\Arduino\hardware\arduino\avr\cores\arduino/Arduino.h \
+ C:\Users\17par\AppData\Local\Temp\VMBuilds\BatteryCharger\nano_atmega328\Debug\arrayOp.h \
+ C:\Users\17par\OneDrive\Documents\Arduino\libraries\TM1637/TM1637Display.h \
+ C:\Users\17par\AppData\Local\Temp\VMBuilds\BatteryCharger\nano_atmega328\Debug\TM1637Display.h
